e1b7ed404586f2d4c320e2b3e1d77d79ac43c03f
Angel Mendoza <1930.kick@gmail.com>
2019-05-13 20:56:18 -0500

    Se cambió el número de registros a guardar

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -12,6 +12,6 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Task::class, 10)->create();
+        factory(App\Task::class, 35)->create();
     }
 }
