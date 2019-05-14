2763ce8d13572f7807098825f1330b5499edb778
Angel Mendoza <1930.kick@gmail.com>
2019-05-13 23:15:45 -0500

    Se hizo la excepción de rutas que no utilizaremos

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
-Route::resource('tasks', 'TaskController')->except(['show']);
+Route::resource('tasks', 'TaskController')->except([
+    'show', 'create', 'edit'
+]);
